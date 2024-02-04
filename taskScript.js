42dc4dc61380b2ae46f18ceb7ab704668b335a56
hannahaerni1 <hannaha@vt.edu>
2024-02-03 22:44:48 -0500

    hell

--- taskScript.js
+++ taskScript.js
@@ -1,5 +1,6 @@
-const listOfTasks = [];
-const listOfEnergy = [];
+var listOfTasks = [];
+var listOfEnergy = [];
+var indexTooMuchEnergy = -1;
 
 function addTask() {
     var taskInput = document.getElementById("taskInput");
@@ -22,7 +23,18 @@ function displayTasks() {
         taskList.removeChild(taskList.firstChild);
     }
 
+    sortTasks();
+
     for (let i = 0; i < listOfTasks.length; i++) {
+        if (i == indexTooMuchEnergy){
+            var spacer = document.createElement('br');
+            var line = document.createElement('hr');
+            var header = document.createElement('h4');
+            header.textContent = "Not Enough Energy";
+            taskList.appendChild(spacer);
+            taskList.appendChild(line);
+            taskList.appendChild(header);
+        }
         var newTask = document.createElement('input');
         newTask.type = "checkbox";
         var newTaskLabel = document.createElement('label');
@@ -35,18 +47,58 @@ function displayTasks() {
     }
 }
 
+function sortTasks() {
+    var newTaskList = [];
+    var newEnergyList = [];
+    
+    var noMoreLeft = false;
+    while (listOfTasks.length != 0) {
+        var mostEnergy = 0;
+        for (let i = 0; i < listOfTasks.length; i++) {
+            if (!noMoreLeft){
+                if (listOfEnergy[i] > listOfEnergy[mostEnergy] && listOfEnergy[i] <= userEnergy) {
+                    mostEnergy = i;
+                }
+            }
+            else {
+                if (listOfEnergy[i] > listOfEnergy[mostEnergy]) {
+                    mostEnergy = i;
+                }
+            }
+            if (i == listOfTasks.length - 1 && listOfEnergy[mostEnergy] > userEnergy && !noMoreLeft){
+                noMoreLeft = true;
+                indexTooMuchEnergy = newTaskList.length;
+                i = 0;
+            }
+        }
+        newTaskList.push(listOfTasks[mostEnergy]);
+        newEnergyList.push(listOfEnergy[mostEnergy]);
+        listOfTasks.splice(mostEnergy, 1);
+        listOfEnergy.splice(mostEnergy, 1);
+    }
+
+    if (!noMoreLeft){
+        indexTooMuchEnergy = -1;
+    }
+
+    listOfTasks = newTaskList;
+    listOfEnergy = newEnergyList;
+}
+
 function removeTask()
 {
     var taskList = document.getElementById("taskList");
     
-    const tasks = taskList.querySelectorAll('input');
-
-    for (let x = 0; x < tasks.length; x++)
+    var tasks = taskList.querySelectorAll('input');
+    var taskLen = tasks.length;
+    for (let x = 0; x < taskLen; x++)
     {
         if (tasks[x].checked)
         {
             listOfTasks.splice(x, 1);
             listOfEnergy.splice(x, 1);
+            x--;
+            taskLen--;
         }
 
     }

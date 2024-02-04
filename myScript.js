27120fd79abfe5b4f5705584ed3519ae60c660f9
hannahaerni1 <hannaha@vt.edu>
2024-02-03 19:14:22 -0500

    new stuff

--- myScript.js
+++ myScript.js
@@ -1,3 +1,5 @@
+const listOfTasks = [];
+
 function addTask() {
     var taskInput = document.getElementById("taskInput");
     var taskText = taskInput.value.trim();
@@ -5,14 +7,21 @@ function addTask() {
     // Check if the input is not empty
     if (taskText !== "") {
         // Create a new list item
-        var newTask = document.createElement("li");
-        newTask.textContent = taskText;
-
-        // Append the new task to the task list
-        var taskList = document.getElementById("taskList");
-        taskList.appendChild(newTask);
-
-        // Clear the input field
-        taskInput.value = "";
+        listOfTasks.push(taskText);
     }
-  }
+    taskInput.value = "";
+}
+
+function displayTasks() {
+    var taskList = document.getElementById("taskList");
+    while (taskList.hasChildNodes()) {
+        taskList.removeChild(taskList.firstChild);
+    }
+
+    for (let i = 0; i < listOfTasks.length; i++) {
+        var newTask = document.createElement('li');
+        newTask.textContent = listOfTasks[i];
+
+        taskList.appendChild(newTask);
+    }
+}
